31702644a1c1b8245e9dee055a4e94cc7d648146
Daniel <daniel-mcintyre@hotmail.com>
2022-09-13 10:43:04 -0400

    create more dynamic routes

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,10 +1,17 @@
+import { useRouter } from 'next/router';
 
 function DetailPage() {
 
-    return (
+    const router = useRouter();
 
+    const newsId = router.query.newsId;
+
+    // send a request to the backend API //
+    // to fetch the news item with newsId //
+
+    return ( 
     <h1>Detail Page</h1>
-    )
-}
+     )
+};
 
 export default DetailPage;
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,9 +1,21 @@
+import { Fragment } from 'react';
+import Link from 'next/link';
 
 function HomePage() {
 
     return (
+    <Fragment>
+    <h1>News Page</h1>
+    <ul>
+        <li>
+        <Link href="/news/nextjs-is-a-great-framework">
+            Next JS is a Great Framework
+        </Link>
+        </li>
+        <li>Something Else</li>
 
-    <h1>Home Page</h1>
+    </ul>
+    </Fragment>
     )
 }
 
